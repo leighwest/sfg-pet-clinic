76571bf996458c035d8475e904b697a3d514b504
Leigh West <leighwest@hotmail.com>
2021-10-15 08:10:04 +1100

    Added base entity.

---
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package sfgpetclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    // Use a box type rather than a primitive (i.e. long) because box types can be null
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
