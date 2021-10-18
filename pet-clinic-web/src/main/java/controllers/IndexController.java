fd7379e60aeb6e3413df1af2525cf24137a236cb
Leigh West <leighwest@hotmail.com>
2021-10-19 07:58:23 +1100

    Added Thymeleaf index page

---
+++ pet-clinic-web/src/main/java/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    // any request that comes into the root context then match to this request mapping and return the index page
+    @RequestMapping({"","/","index","index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
