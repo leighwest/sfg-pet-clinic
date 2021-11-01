101afad3b5b066c9848e40fe3e5dcd3bca85ab96
Leigh West <leighwest@hotmail.com>
2021-11-02 08:43:33 +1100

    Added PetTypes to bootstrap DataLoader

---
+++ pet-clinic-data/src/main/java/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package springframework.sfgpetclinic.services;
+
+import springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
--- pet-clinic-web/src/main/java/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/springframework/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package springframework.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import springframework.sfgpetclinic.model.Owner;
+import springframework.sfgpetclinic.model.PetType;
 import springframework.sfgpetclinic.model.Vet;
 import springframework.sfgpetclinic.services.OwnerService;
+import springframework.sfgpetclinic.services.PetTypeService;
 import springframework.sfgpetclinic.services.VetService;
 
 
@@ -13,14 +15,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
