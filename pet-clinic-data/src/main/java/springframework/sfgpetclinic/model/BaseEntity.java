ed34c66eda1a1e1906427c3050858fd8fafdba9f
Leigh West <leighwest@hotmail.com>
2021-11-17 07:45:57 +1100

    Updated base entity with JPA annotations.

--- pet-clinic-data/src/main/java/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,10 +1,16 @@
 package springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-    // Use a box type rather than a primitive (i.e. long) because box types can be null
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
--- pet-clinic-web/src/main/java/springframework/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/springframework/controllers/OwnerController.java
@@ -22,4 +22,6 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+
 }
